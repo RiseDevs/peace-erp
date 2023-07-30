0cf66e0a190fd98fc8910e83d09703438bc733eb
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-07-30 15:01:53 -0300

    Enum file correction

--- peace-erp/Domain/Entities/BilingTypes.cs
+++ peace-erp/Domain/Entities/BilingTypes.cs
@@ -7,8 +7,3 @@ public class BilingTypes : BaseEntity
     public string Description { get; set; }
     public ShownAt ShownAt { get; set; }
 }
-public enum ShownAt
-{
-    IncomeOutcome = 1,
-    AccountBook = 2,
-}
---
+++ peace-erp/Domain/Entities/ShownAtEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace peace_erp.Domain.Entities;
+
+public enum ShownAt
+{
+    IncomeOutcome = 1,
+    AccountBook = 2,
+}
