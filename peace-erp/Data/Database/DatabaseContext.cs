16f66c77264d8b43088018adcbf8bff4d414f03c
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-07-30 14:53:49 -0300

    Entity framework initial settings

---
+++ peace-erp/Data/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using peace_erp.Domain.Entities;
+
+namespace peace_erp.Data.Database;
+
+public class DatabaseContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("peace-erp");
+    }
+
+    public DbSet<AccountTypes> AccountTypes { get; set; }
+    public DbSet<BilingTypes> BilingTypes { get; set; }
+    public DbSet<DocumentTypes> DocumentTypes { get; set; }
+    public DbSet<PersonTypes> PersonTypes { get; set; }
+    public DbSet<PersonLegals> PersonLegals { get; set; }
+}
